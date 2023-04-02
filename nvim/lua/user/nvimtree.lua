6a9543d5ab42685c346dc7558a9e8c4a8c0c73fe
aliyss <silyas.bieri@lagomio.com>
2023-04-02 12:26:33 +0200

    Colorscheme editing

--- nvim/lua/user/nvimtree.lua
+++ nvim/lua/user/nvimtree.lua
@@ -107,10 +107,8 @@ local function open_nvim_tree(data)
 	-- change to the directory
 	if directory then
 		vim.cmd.cd(data.file)
+		require("nvim-tree.api").tree.open()
 	end
-
-	-- open the tree
-	require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
